c9f37c865fad8de3b7f8b166299a4cb4b43b4190
Dayoming <jdayoung1204@naver.com>
2024-10-09 20:03:59 +0900

    chore: day7 과제 패키지 생성

---
+++ src/main/java/cleancode/studycafe/day7/StudyCafeApplication.java
@@ -0,0 +1,10 @@
+package cleancode.studycafe.day7;
+
+public class StudyCafeApplication {
+
+    public static void main(String[] args) {
+        StudyCafePassMachine studyCafePassMachine = new StudyCafePassMachine();
+        studyCafePassMachine.run();
+    }
+
+}
